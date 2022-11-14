bb0e87713a7ddf2e2f09a2289aad0d262f44b0c6
Imran Ahmed <ImranAhmed12>
2022-11-14 13:54:14 +0530

    first commit

---
+++ src/Heading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <div style={{textAlign:"center",fontSize:"20px", backgroundColor:"gray",color:"white", fontFamily:"cursive"}}>PHOTO  SEARCH  APPLICATION</div>
+  )
+}
+
+export default Heading
